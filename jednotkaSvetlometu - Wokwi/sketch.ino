a7bcea805b7655e0da6f7758987be3f5c953c4dc
ondrejbrousil <79591299+ondrejbrousil@users.noreply.github.com>
2025-03-08 23:52:55 +0100

    Soubory projektu ve Wokwi.
    Přidány obrysová a dálková světla - prozatím nefunkční.

--- jednotkaSvetlometu - Wokwi/sketch.ino
+++ jednotkaSvetlometu - Wokwi/sketch.ino
@@ -1,9 +1,41 @@
-int inObrys = 6;
-int inDalkove = 5;
-int inLeve = 4;
-int inPrave = 3;
-int inVystrazne = 2;
+/*
 
+Řídící jednotka svetlometu - firmware
+- stále ve vývoji
+- funkční animace směrovek, není možné je spustit přepínačemi
+- nefunkční přepínače, obrysová a dálková světla
+
+https://github.com/ondrejbrousil/jednotkaSvetlometu$0
+
+*/
+
+//Tlacitka a spinace
+int pinObrys = 8;
+int pinDalkove = 7;
+int pinLeve = 4;
+int pinPrave = 3;
+int pinVystrazne = 2;
+
+int stavObrys = 0;
+int stavDalkove = 0;
+int stavLeve = 0;
+int stavPrave = 0;
+int stavVystrazne = 0;
+
+//Obrys
+int obrysDimm = 0;
+int obrysDimmKrok = 20;
+int obrysDimmKrokProdleva = 100;
+int vystupObrys = 5;
+
+//Dalkove
+int dalkoveDimm = 0;
+int dalkoveDimmKrok = 20;
+int dalkoveDimmKrokProdleva = 100;
+int vystupDalkove = 6;
+
+
+//Smerorvky
 int clock = 12;
 int latchLeve = 11;
 int dataLeve = 13;
@@ -25,12 +57,12 @@ void setup() {
 }
 
 void smerovkyLeveVoid() {
-  for (smerovkyLeve; smerovkyLeve <= 9; smerovkyLeve++) {
+  for (smerovkyLeve; smerovkyLeve <= 8; smerovkyLeve++) {
     smerovkyLeveZobrazeni();
     delay(smerovkyProdleva);
   }
 
-  if (smerovkyLeve > 9) {
+  if (smerovkyLeve > 8) {
     smerovkyLeve = 0;
   }
 }
@@ -39,50 +71,45 @@ void smerovkyLeveZobrazeni() {
   switch (smerovkyLeve) {
     case 0:
       digitalWrite(latchLeve, LOW);
-      shiftOut(dataLeve, clock, MSBFIRST, 0);
+      shiftOut(dataLeve, clock, MSBFIRST, 1);
       digitalWrite(latchLeve, HIGH);
       break;
     case 1:
       digitalWrite(latchLeve, LOW);
-      shiftOut(dataLeve, clock, MSBFIRST, 0);
+      shiftOut(dataLeve, clock, MSBFIRST, 1);
       digitalWrite(latchLeve, HIGH);
       break;
     case 2:
       digitalWrite(latchLeve, LOW);
-      shiftOut(dataLeve, clock, MSBFIRST, 1);
+      shiftOut(dataLeve, clock, MSBFIRST, 3);
       digitalWrite(latchLeve, HIGH);
       break;
     case 3:
       digitalWrite(latchLeve, LOW);
-      shiftOut(dataLeve, clock, MSBFIRST, 3);
+      shiftOut(dataLeve, clock, MSBFIRST, 7);
       digitalWrite(latchLeve, HIGH);
       break;
     case 4:
       digitalWrite(latchLeve, LOW);
-      shiftOut(dataLeve, clock, MSBFIRST, 7);
+      shiftOut(dataLeve, clock, MSBFIRST, 15);
       digitalWrite(latchLeve, HIGH);
       break;
     case 5:
       digitalWrite(latchLeve, LOW);
-      shiftOut(dataLeve, clock, MSBFIRST, 15);
+      shiftOut(dataLeve, clock, MSBFIRST, 31);
       digitalWrite(latchLeve, HIGH);
       break;
     case 6:
       digitalWrite(latchLeve, LOW);
-      shiftOut(dataLeve, clock, MSBFIRST, 31);
+      shiftOut(dataLeve, clock, MSBFIRST, 63);
       digitalWrite(latchLeve, HIGH);
       break;
     case 7:
       digitalWrite(latchLeve, LOW);
-      shiftOut(dataLeve, clock, MSBFIRST, 63);
-      digitalWrite(latchLeve, HIGH);
-      break;
-    case 8:
-      digitalWrite(latchLeve, LOW);
       shiftOut(dataLeve, clock, MSBFIRST, 127);
       digitalWrite(latchLeve, HIGH);
       break;
-    case 9:
+    case 8:
       digitalWrite(latchLeve, LOW);
       shiftOut(dataLeve, clock, MSBFIRST, 255);
       digitalWrite(latchLeve, HIGH);
@@ -91,12 +118,12 @@ void smerovkyLeveZobrazeni() {
 }
 
 void smerovkyPraveVoid() {
-  for (smerovkyPrave; smerovkyPrave <= 9; smerovkyPrave++) {
+  for (smerovkyPrave; smerovkyPrave <= 8; smerovkyPrave++) {
     smerovkyPraveZobrazeni();
     delay(smerovkyProdleva);
   }
 
-  if (smerovkyPrave > 9) {
+  if (smerovkyPrave > 8) {
     smerovkyPrave = 0;
   }
 }
@@ -105,50 +132,45 @@ void smerovkyPraveZobrazeni() {
   switch (smerovkyPrave) {
     case 0:
       digitalWrite(latchPrave, LOW);
-      shiftOut(dataPrave, clock, MSBFIRST, 0);
+      shiftOut(dataPrave, clock, MSBFIRST, 1);
       digitalWrite(latchPrave, HIGH);
       break;
     case 1:
       digitalWrite(latchPrave, LOW);
-      shiftOut(dataPrave, clock, MSBFIRST, 0);
+      shiftOut(dataPrave, clock, MSBFIRST, 1);
       digitalWrite(latchPrave, HIGH);
       break;
     case 2:
       digitalWrite(latchPrave, LOW);
-      shiftOut(dataPrave, clock, MSBFIRST, 1);
+      shiftOut(dataPrave, clock, MSBFIRST, 3);
       digitalWrite(latchPrave, HIGH);
       break;
     case 3:
       digitalWrite(latchPrave, LOW);
-      shiftOut(dataPrave, clock, MSBFIRST, 3);
+      shiftOut(dataPrave, clock, MSBFIRST, 7);
       digitalWrite(latchPrave, HIGH);
       break;
     case 4:
       digitalWrite(latchPrave, LOW);
-      shiftOut(dataPrave, clock, MSBFIRST, 7);
+      shiftOut(dataPrave, clock, MSBFIRST, 15);
       digitalWrite(latchPrave, HIGH);
       break;
     case 5:
       digitalWrite(latchPrave, LOW);
-      shiftOut(dataPrave, clock, MSBFIRST, 15);
+      shiftOut(dataPrave, clock, MSBFIRST, 31);
       digitalWrite(latchPrave, HIGH);
       break;
     case 6:
       digitalWrite(latchPrave, LOW);
-      shiftOut(dataPrave, clock, MSBFIRST, 31);
+      shiftOut(dataPrave, clock, MSBFIRST, 63);
       digitalWrite(latchPrave, HIGH);
       break;
     case 7:
       digitalWrite(latchPrave, LOW);
-      shiftOut(dataPrave, clock, MSBFIRST, 63);
-      digitalWrite(latchPrave, HIGH);
-      break;
-    case 8:
-      digitalWrite(latchPrave, LOW);
       shiftOut(dataPrave, clock, MSBFIRST, 127);
       digitalWrite(latchPrave, HIGH);
       break;
-    case 9:
+    case 8:
       digitalWrite(latchPrave, LOW);
       shiftOut(dataPrave, clock, MSBFIRST, 255);
       digitalWrite(latchPrave, HIGH);
@@ -157,6 +179,32 @@ void smerovkyPraveZobrazeni() {
 
 }
 
+/* 
+
+P O Z N A M K A : aktualne ve stadiu vyvoje
+
+void obrysVoid() {
+
+  int obrysPricteni = obrysDimm+obrysDimmKrok;
+  Serial.print("vysledek: ");
+  Serial.println(obrysPricteni);
+
+  for (obrysDimm; obrysDimm <= 255; obrysDimm+obrysDimmKrok) {
+    digitalWrite(vystupObrys, obrysDimm);
+    delay(obrysDimmKrokProdleva);
+    Serial.println(obrysDimm);
+  }
+}
+
+void cteniVstupu() {
+  stavObrys = digitalRead(pinObrys);
+
+  if(stavObrys) {
+    
+  }
+}
+*/
+
 void loop() {
-  smerovkyLeveVoid();
+  smerovkyPraveVoid();
 }
